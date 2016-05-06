0a49802b418cd8a2b61ad372b54f8f73f79bce82
Sagar Arora <supriyaarora@SupriyaorasiMac.home>
2016-05-06 16:11:16 -0400

    Multiplier App

--- multiplier/ViewController.swift
+++ multiplier/ViewController.swift
@@ -19,7 +19,120 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var multiplier = 0
+    var currentMultiplying = 0
+    var Max = 50
+    
+    @IBOutlet weak var MultiplesTextField: UITextField!
+    @IBOutlet weak var CurrentMultiplesText: UILabel!
+    @IBOutlet weak var MultiplesLogo: UIImageView!
+    @IBOutlet weak var AddBtn: UIButton!
+    @IBOutlet weak var PlayBtn: UIButton!
+
+    @IBAction func playButton(sender: AnyObject) {
+        if (MultiplesTextField.text != nil && MultiplesTextField.text != "") {
+       
+        MultiplesLogo.hidden = true
+        MultiplesTextField.hidden = true
+        AddBtn.hidden = false
+        PlayBtn.hidden = true
+        CurrentMultiplesText.hidden = false
+            
+            multiplier = Int(MultiplesTextField.text!)!
+            currentMultiplying = 0
+            
+            
+            
+          
+        
+        
+        }
+        
+   
+    
+    }
+    
+    
+
+   
+    
+    func updateLbl (oldSum: Int, mul: Int, newSum: Int) {
+        
+        CurrentMultiplesText.text = "\(oldSum) + \(mul) = \(newSum)"
+            
+    }
+    
+
+    @IBAction func AddMultiplier(sender: AnyObject) {
+        let newSum = multiplier + currentMultiplying
+       
+        currentMultiplying+=multiplier
+         updateLbl(currentMultiplying, mul: multiplier, newSum: newSum)
+        // taking in a current number thats being multiplied, the multiplier, and the new sum
+        // The new sum always stays constant
+        // the currentMultiplying changes based on the muliplier 
+        
+        if isGameOver() {
+            
+            restartGame()
+            
+        }
+        
+    }
+    
+    func isGameOver() -> Bool {
+        if (currentMultiplying >= Max) {
+            
+            return true
+            
+        }
+
+        else {
+        return false
+        }
+        
+    }
+    
+    func resetLbl () {
+        CurrentMultiplesText.text = "Click Add new to Add Multiples!"
+        
+    }
+    
+    func restartGame() {
+        multiplier = 0
+        MultiplesLogo.hidden = false
+        MultiplesTextField.hidden = false
+        AddBtn.hidden = true
+        PlayBtn.hidden = false
+        CurrentMultiplesText.hidden = true
+        resetLbl()
+      
+
+        
+        
+    }
+    
+        
+        
+        
+       
+        
+            
+            
+        
+            
+        
+        
+        
+        
+    
+    }
 
 
-}
+
+   
+
+
+
 
